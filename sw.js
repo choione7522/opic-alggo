fb2469d1c8b80df7a5dbb5f11f60ee5aaf5cc698
choione7522 <49219362+choione7522@users.noreply.github.com>
2026-09-07 11:34:34 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,12 +1,27 @@
-const CACHE = 'opic-v1';
+const CACHE = 'opic-v2';
 const FILES = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
 });
 
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    )).then(() => self.clients.claim())
+  );
+});
+
+// 네트워크 우선: 온라인이면 항상 최신 파일을 받아오고, 오프라인일 때만 캐시로 대체한다.
+// (예전 버전은 캐시가 있으면 무조건 캐시만 써서, 서버를 업데이트해도 설치된 앱에는 영영 반영되지 않았음)
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+    fetch(e.request).then(res => {
+      var copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy));
+      return res;
+    }).catch(() => caches.match(e.request))
   );
 });
